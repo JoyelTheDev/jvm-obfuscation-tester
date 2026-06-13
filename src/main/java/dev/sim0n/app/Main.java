ecaed2d703a7c8f7e98524d986044532e4c80659
JoyelTheDev <222600183+JoyelTheDev@users.noreply.github.com>
2026-06-13 08:03:08 +0530

    Add test execution to main application flow

--- src/main/java/dev/sim0n/app/Main.java
+++ src/main/java/dev/sim0n/app/Main.java
@@ -9,5 +9,6 @@ public class Main {
     public static void main(String[] args) {
         Application application = new Application();
         application.run();
+        Tests.runTests();
     }
 }
